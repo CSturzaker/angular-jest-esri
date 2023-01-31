0482673fe0a84466a2fc612ce54cca4a2ae77ff3
Craig Sturzaker <craig.sturzaker@arup.com>
2023-01-31 09:25:54 +0000

    feat(esri): add arcgis api

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, OnDestroy, OnInit, ViewChild } from '@angular/core';
+
+import { MapService } from './services/map.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit, OnDestroy {
   title = 'ng-jest';
+  
+  public view: any = null;
+  private mapService: any;
+  // The <div> where we will place the map
+  @ViewChild('mapViewNode', { static: true }) public mapViewEl!: ElementRef;
+
+  ngOnInit(): any {
+    // Initialize MapView and return an instance of MapView
+    this.mapService = new MapService(this.mapViewEl);
+    this.mapService.initializeMap().then(() => {
+      console.log("Map is loaded");
+    });
+  }
+
+  ngOnDestroy(): void {
+    if (this.view) {
+      // destroy the map view
+      this.view.destroy();
+    }
+  }
 }
---
+++ src/app/services/map.service.ts
@@ -0,0 +1,33 @@
+import { ElementRef, Injectable } from '@angular/core';
+
+import WebMap from '@arcgis/core/WebMap';
+import MapView from '@arcgis/core/views/MapView';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class MapService {
+  public mapViewEl: ElementRef<any>;
+  public view: any = null;
+
+  constructor(mapViewEl: ElementRef) {
+    this.mapViewEl = mapViewEl;
+  }
+
+  initializeMap(): Promise<any> {
+    let container = this.mapViewEl.nativeElement;
+
+    const webmap = new WebMap({
+      portalItem: {
+        id: 'aa1d3f80270146208328cf66d022e09c',
+      },
+    });
+
+    this.view = new MapView({
+      container,
+      map: webmap,
+    });
+
+    return this.view.when();
+  }
+}
